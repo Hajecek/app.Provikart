0cd118af6485cd80fcda937de68dc47f7ede0bcf
Hajecek <haj8cek@gmail.com>
2026-02-26 15:45:02 +0100

    edit

--- Provikart/ContentView.swift
+++ Provikart/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
+enum Tabs: Hashable {
+    case home
+    case profile
+    case settings
+    case search
+}
+
 struct ContentView: View {
+    @State var selectedTab: Tabs = .home
+    @State var searchString = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $selectedTab) {
+            Tab("Home", systemImage: "house", value: .home) {
+                Color.green.ignoresSafeArea()
+            }
+            
+            Tab("Profile", systemImage: "person", value: .profile) {
+                Color.orange.ignoresSafeArea()
+            }
+            
+            Tab("Settings", systemImage: "gearshape", value: .settings) {
+                Color.blue.ignoresSafeArea()
+            }
+            
+            Tab(value: .search, role: .search) {
+                NavigationStack {
+                    List {
+                        Text("Search screen")
+                    }
+                    .navigationTitle("Search")
+                    .searchable(text: $searchString)
+                }
+            }
         }
-        .padding()
     }
 }
 
